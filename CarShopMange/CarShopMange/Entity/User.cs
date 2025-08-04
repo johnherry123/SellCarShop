a2e6bce0a6355e505175a018578ff09e2e0834a5
JohnHenry <xankhbm1@gmail.com>
2025-08-04 11:24:39 +0900

    add project and add lib

---
+++ CarShopMange/CarShopMange/Entity/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopMange.Entity
+{
+    class User
+    {
+        [Key]
+        [Required(ErrorMessage ="User Id is required.")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int userId { get; set; }
+        [Required(ErrorMessage ="Username is required.")]
+        public string userName { get; set; }
+        [Required(ErrorMessage ="Email is required.")]
+        [EmailAddress(ErrorMessage ="")]
+        public string email { get; set; }
+    }
+}
---
+++ CarShopMange/CarShopMange/Entity/UserRole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopMange.Entity
+{
+   
+    class UserRole
+    {
+        [Required(ErrorMessage ="User ID is required.")]
+        [ForeignKey("userID")]
+        public int userId { get; set; }
+        [Required(ErrorMessage ="Role is require")]
+        [EnumDataType(typeof(Role),ErrorMessage ="Invalid Role")]
+        public Role role { get; set; }
+
+    }
+}
