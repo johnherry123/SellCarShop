6ff2fbe4ec23fb989d0f02ab92bdbe97e6169975
JohnHenry <xankhbm1@gmail.com>
2025-08-11 16:19:48 +0900

    add employee

---
+++ CarShopMange/CarShopMange/Helper/FractionWidthConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CarShopMange.Helper
+{
+    public class FractionWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double total && parameter is string p)
+            {
+                var parts = p.Split(';');
+                var frac = double.Parse(parts[0], NumberStyles.Any, CultureInfo.InvariantCulture);
+                var subtract = parts.Length > 1
+                    ? double.Parse(parts[1], NumberStyles.Any, CultureInfo.InvariantCulture)
+                    : 0d;
+
+                var usable = Math.Max(0, total - subtract - 35);
+                return Math.Max(50, usable * frac);          
+            }
+            return 100d;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
